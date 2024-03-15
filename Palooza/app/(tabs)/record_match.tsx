3cd921b917392cf5f56089c130df56bd6b50ba1c
antoniolang1107 <TonylsLame@users.noreply.github.com>
2024-03-15 15:21:50 -0700

    Record match accepts user input for POST to backend

--- Palooza/app/(tabs)/record_match.tsx
+++ Palooza/app/(tabs)/record_match.tsx
@@ -1,24 +1,80 @@
-import { StyleSheet } from 'react-native';
-import { Text, View } from '../../components/Themed';
+import { Button, StyleSheet, View, Text, TextInput } from 'react-native';
+import { useState } from 'react';
+// import { Text, View } from '../../components/Themed';
+
+// TODO convert input fields into a reuseable component
+function RecordTextField(myValue: string, setValue: React.SetStateAction<string>, placeholderText: string) {
+  return (
+  <TextInput
+    style={styles.input}
+    placeholder={placeholderText}
+    // onChangeText={myValue => setValue(myValue)}
+  />
+  );
+}
+
+function getNumbers(text: string) {
+  return text.replace(/[^0-9]/g, '')
+}
 
 export default function RecordMatch() {
+  const [name1, setName1] = useState("");
+  const [name2, setName2] = useState("");
+  const [win_val, setWinVal] = useState("");
+  const [score1, setScore1] = useState("");
+  const [score2, setScore2] = useState("");
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Record Match</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View style={styles.separator}/>
+      {/* <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" /> */}
       {/* IDEA: query for names, populate dropdown */}
       {/* use radio button for win-con value */}
       {/* ensure competitor 1 != competitor 2 */}
       {/* check for valid score */}
       {/* the dropdown is a non-essential feature; make later */}
-      <button onClick={getNames}>Get Names</button>
-      <input id='competitor 1' placeholder='competitor 1'></input>
-      <input id='competitor 2' placeholder='competitor 2'></input>
-      <input id='win val' placeholder='win val'></input>
-      <input id='score 1' placeholder='score 1'></input>
-      <input id='score 2' placeholder='score 2'></input>
-      <button onClick={submit}>Submit Details</button>
-      {/* <Form></Form> */}
+      <Button onPress={getNames} title='Get Names'/>
+      <TextInput
+        style={styles.input}
+        placeholder='Competitor 1'
+        onChangeText={(value) => setName1(value)}
+        editable={true}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Competitor 2'
+        onChangeText={(value) => setName2(value)}
+        editable={true}
+      />
+      {/* <RecordTextField value={name1} setValue={setName1} placeholderText={"Temp"}/> */}
+      <TextInput
+        style={styles.input}
+        placeholder='Match Score (i.e. 11, 21)'
+        onChangeText={(value) => setWinVal(getNumbers(value))}
+        inputMode='numeric'
+        editable={true}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Score 1'
+        onChangeText={(value) => setScore1(getNumbers(value))}
+        inputMode='numeric'
+        editable={true}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder='Score 2'
+        onChangeText={(value) => setScore2(getNumbers(value))}
+        inputMode='numeric'
+        editable={true}
+      />
+      <Button
+        title='Submit Details'
+        onPress={()=>submit(name1, name2, win_val, score1, score2)}
+        accessibilityLabel='Submit entered details'
+      />
+      {/* <button onClick={()=>submit(name1, name2, win_val, score1, score2)}>Submit Details</button> */}
     </View>
   );
 }
@@ -28,19 +84,18 @@ async function getNames() {
   console.log(names)
 }
 
-async function submit() {
+async function submit(competitor_name_1: string, competitor_name_2: string,
+  win_val: string, score_1: string, score_2: string) {
 
-  // const firstName = document.getElementById("firstName") as HTMLInputElement;
-  
   fetch('http://127.0.0.1:5000/update', {
     method: 'post',
     headers: {'Content-Type':'application/json'},
     body: JSON.stringify({
-      "competitor_1": "temp",
-      "competitor_2": "temp",
-      "win_val": 21,
-      "score_1": 21,
-      "score_2": 19,
+      "competitor_1": competitor_name_1,
+      "competitor_2": competitor_name_2,
+      "win_val": win_val,
+      "score_1": score_1,
+      "score_2": score_2,
     })
   });
 }
@@ -70,4 +125,14 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  input: {
+    borderColor: "grey",
+    borderWidth: 1,
+    borderRadius: 10,
+    backgroundColor: "ffffff",
+    padding: 5,
+  },
+  button: {
+    padding: 5,
+  }
 });
